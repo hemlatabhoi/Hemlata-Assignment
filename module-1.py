cfbb5a9876845b21127a1eaf45f56ee1a3858edc
Hemlata bhoi <118828723+hemlatabhoi@users.noreply.github.com>
2023-04-23 09:15:57 -0500

    Add files via upload

---
+++ module-1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+module-1
+
+
+# In[ ]:
+
+
+#Question 1: What is Software?
+
+Software is a set of instructions, data or programs used to operate computers and execute specific tasks. 
+It is the opposite of hardware, which describes the physical aspects of a computer. 
+Software is a generic term used to refer to applications, scripts and programs that run on a device. 
+It can be thought of as the variable part of a computer, while hardware is the invariable part.
+
+
+# In[ ]:
+
+
+#Question 2: What are the types of Applications?
+
+An application is a program that runs on a computer, mobile phone/tablet, or through a browser 
+(in this case, you need constant access to the Internet).
+Although applications are no different from computer programs, this term is usually used to 
+refer to simplified programs intended for the average user (or other programs).
+    
+
+
+# In[ ]:
+
+
+#Question 3: What is programing?
+
+Programming is the process of creating a set of instructions that tell a computer how to perform a task. 
+Programming can be done using a variety of computer programming languages, 
+such as JavaScript, Python, and C++
+
+
+# In[ ]:
+
+
+#Question 4: What is Python?
+
+Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis.
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+Python consistently ranks as one of the most popular programming languages.
+
